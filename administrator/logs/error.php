10c87aa66c2eb913324e1227089c251a25e85d6e
simo <simo@simo.com>
2021-12-12 11:37:44 +0100

    mi commit otro 121221

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-12-12 10:36:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-12-12T10:36:17+00:00	INFO 80.91.82.9	joomlafailure	Empty password not allowed.
